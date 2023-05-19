b94edcb4ea95075918adc12cc80d5e6322433461
kostchei <56498970+kostchei@users.noreply.github.com>
2023-05-19 18:27:51 +0800

    more stuff

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ function App() {
   const [baseEthos, setBaseEthos] = useState('None');
   const [baseMoral, setBaseMoral] = useState('None');
   const [endAlignment, setEndAlignment] = useState('Neutral Neutral');
+  const [piety, setPiety] = useState(0);
+  const [factionalReputation, setFactionalReputation] = useState(0);
+  const [selectedPairs, setSelectedPairs] = useState([]);
 
-  // Declare calculateEndAlignment function here...
+  const virtuesVices = [
+    { virtue: "Chaste", vice: "Lustful" },
+    { virtue: "Energetic", vice: "Lazy" },
+    { virtue: "Forgiving", vice: "Vengeful" },
+    { virtue: "Generous", vice: "Selfish" },
+    { virtue: "Honest", vice: "Deceitful" },
+    { virtue: "Just", vice: "Arbitrary" },
+    { virtue: "Merciful", vice: "Cruel" },
+    { virtue: "Modest", vice: "Proud" },
+    { virtue: "Prudent", vice: "Reckless" },
+    { virtue: "Temperate", vice: "Indulgent" },
+    { virtue: "Trusting", vice: "Suspicious" },
+    { virtue: "Valorous", vice: "Cowardly" }
+  ];
+
   const calculateEndAlignment = () => {
     let ethosResult, moralResult;
     let randomValueEthos = Math.random() * 100;
@@ -32,6 +49,84 @@ function App() {
       moralResult = randomValueMoral < 5 ? 'Good' : randomValueMoral < 32 ? 'Neutral' : 'Evil';
     }
 
+  let pietyValue = Math.abs(2 * Math.floor(Math.random() * 20 + 1) - 20);
+  let factionalReputationValue = Math.abs(2 * Math.floor(Math.random() * 50 + 1) - 50);
+
+  setPiety(pietyValue);
+  setFactionalReputation(factionalReputationValue);
+
+  setEndAlignment(ethosResult + ' ' + moralResult);
+
+  let pairsCopy = [...virtuesVices];
+  const selected = [];
+
+  switch (moralResult) {
+    case 'Good':
+      for(let i = 0; i < 2; i++) { // select two virtues
+        let index = Math.floor(Math.random() * pairsCopy.length);
+        selected.push({
+          ...pairsCopy[index], 
+          virtueValue: Math.floor(Math.random() * 6) + 15,
+          viceValue: undefined
+        });
+        pairsCopy.splice(index, 1);
+      }
+      // select one vice from the remaining pairs
+      let index = Math.floor(Math.random() * pairsCopy.length);
+      selected.push({
+        ...pairsCopy[index], 
+        virtueValue: undefined,
+        viceValue: Math.floor(Math.random() * 6) + 15
+      });
+      break;
+    case 'Neutral':
+      for(let i = 0; i < 2; i++) { // select one virtue and one vice
+        let index = Math.floor(Math.random() * pairsCopy.length);
+        if (i === 0) {
+          selected.push({
+            ...pairsCopy[index], 
+            virtueValue: Math.floor(Math.random() * 6) + 15,
+            viceValue: undefined
+          });
+        } else {
+          selected.push({
+            ...pairsCopy[index], 
+            virtueValue: undefined,
+            viceValue: Math.floor(Math.random() * 6) + 15
+          });
+        }
+        pairsCopy.splice(index, 1);
+      }
+      break;
+    case 'Evil':
+      for(let i = 0; i < 2; i++) { // select two vices
+        let index = Math.floor(Math.random() * pairsCopy.length);
+        selected.push({
+          ...pairsCopy[index], 
+          virtueValue: undefined,
+          viceValue: Math.floor(Math.random() * 6) + 15
+        });
+        pairsCopy.splice(index, 1);
+      }
+      // select one virtue from the remaining pairs
+      index = Math.floor(Math.random() * pairsCopy.length);
+      selected.push({
+        ...pairsCopy[index], 
+        virtueValue: Math.floor(Math.random() * 6) + 15,
+        viceValue: undefined
+      });
+      break;
+    default:
+      setSelectedPairs([]);
+  }
+
+  setSelectedPairs(selected);
+}; 
+
+
+
+
+
   return (
     <div className="App">
       <select value={baseEthos} onChange={e => setBaseEthos(e.target.value)}>
@@ -52,8 +147,21 @@ function App() {
 
       <h3>End Alignment:</h3>
       <p>{endAlignment}</p>
+
+      <h3>Character Traits:</h3>
+      <ul>
+        {selectedPairs.map(pair => 
+          <li key={pair.virtue}>
+            {pair.virtue}: {pair.virtueValue || "N/A"}, {pair.vice}: {pair.viceValue || "N/A"}
+          </li>)}
+      </ul>
+      
+      <h3>Piety:</h3>
+      <p>{piety}</p>
+
+      <h3>Factional Reputation:</h3>
+      <p>{factionalReputation}</p>
     </div>
   );
 }
-
 export default App;
